4928020a26cf5384a419adf2466d5985fdd3cce4
Dennis van der Veeke <MrDienns@dyescape.com>
2019-02-15 23:10:49 +0100

    improved comments on the Events constant class

--- src/main/java/com/dyescape/dyescapebot/constant/Events.java
+++ src/main/java/com/dyescape/dyescapebot/constant/Events.java
@@ -1,9 +1,15 @@
 package com.dyescape.dyescapebot.constant;
 
+/**
+ * Class containing constants of all addresses one can subscribe
+ * to, or publish messages on when dealing with this application.
+ * @author Dennis van der Veeke
+ * @since 0.1.0
+ */
 public final class Events {
 
     // -------------------------------------------- //
-    // LOGGER
+    // CONSTANTS
     // -------------------------------------------- //
 
     public static final String STARTUP_EVENT = "STARTUP_EVENT";
