112140525fb30db51e9085e64f92dc4827637a41
Dennis van der Veeke <mrdienns@dyescape.com>
2020-08-13 19:28:11 +0200

    added discord command to change prefix

---
+++ discord/src/main/java/com/dyescape/bot/discord/command/configuration/ConfigurationCommand.java
@@ -0,0 +1,34 @@
+package com.dyescape.bot.discord.command.configuration;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.JDACommandEvent;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Subcommand;
+import com.dyescape.bot.data.entity.ServerEntity;
+import com.dyescape.bot.data.repository.ServerRepository;
+
+import java.util.Optional;
+
+@CommandAlias("configuration")
+public class ConfigurationCommand extends BaseCommand {
+
+    private final ServerRepository serverRepository;
+
+    public ConfigurationCommand(ServerRepository serverRepository) {
+        this.serverRepository = serverRepository;
+    }
+
+    @Subcommand("setprefix")
+    public void setPrefix(JDACommandEvent e, String prefix) {
+        Optional<ServerEntity> query = this.serverRepository.findById(e.getEvent().getGuild().getId());
+        ServerEntity serverEntity;
+        if (query.isEmpty()) {
+            serverEntity = new ServerEntity(e.getEvent().getGuild().getId(), prefix);
+        } else {
+            serverEntity = query.get();
+        }
+        serverEntity.setCommandPrefix(prefix);
+        this.serverRepository.save(serverEntity);
+        e.getEvent().getChannel().sendMessage(String.format("Changed command prefix to %s", prefix)).submit();
+    }
+}
